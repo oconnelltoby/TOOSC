a33f334cbf4f6fb53edfe4cf32d555c976ef8121
Toby O'Connell <tobyoconnell@hotmail.com>
2020-12-19 22:18:49 +0000

    Prevent load from misaligned bytes errors
    - Use `Data.subdata` in instances where `UnsafeRawBufferPointer.load` is used
    - By creating a new Data object, the load type always start at an index of 0

--- Sources/TOOSC/OSCArgument/Float32.swift
+++ Sources/TOOSC/OSCArgument/Float32.swift
@@ -16,8 +16,8 @@ extension Float32: OSCArgument {
     
     public init?(oscData: Data, index: inout Int) {
         let size = MemoryLayout<Self>.size
+        let oscData = oscData.subdata(in: index ..< index + size)
         guard oscData.count >= size else { return nil }
-        let oscData = oscData[index ..< index + size]
         
         
         let bigEndian = oscData.withUnsafeBytes { $0.load(as: UInt32.self) }
--- Sources/TOOSC/OSCArgument/Int32.swift
+++ Sources/TOOSC/OSCArgument/Int32.swift
@@ -16,8 +16,8 @@ extension Int32: OSCArgument {
     
     public init?(oscData: Data, index: inout Int) {
         let size = MemoryLayout<Self>.size
+        let oscData = oscData.subdata(in: index ..< index + size)
         guard oscData.count >= size else { return nil }
-        let oscData = oscData[index ..< index + size]
         
         self.init(bigEndian: oscData.withUnsafeBytes { $0.load(as: Int32.self) })
         index += size
